b6ba2c3d85e30659587794f3350602139f38055b
ramusus <ramusus@gmail.com>
2018-02-17 17:53:12 -0500

    update settings

--- cinemanio/settings.py
+++ cinemanio/settings.py
@@ -150,8 +150,13 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # celery
 CELERY_HOST = '127.0.0.1'
