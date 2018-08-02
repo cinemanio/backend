7ca5042d23d694f1a80b5884b1637df05529bea0
Shifrin Boris <boris.shifrin2@nih.org>
2018-08-02 00:30:00 -0400

    fix prospector

--- cinemanio/sites/imdb/models.py
+++ cinemanio/sites/imdb/models.py
@@ -14,11 +14,13 @@ class UrlMixin:
 class ImdbMovieManager(models.Manager):
     def create_for(self, movie):
         if not movie.title or not movie.year:
-            raise ValueError("To be able search movie in IMDb it should has at lease title and year")
+            raise ValueError("To be able search IMDb movie it should has at lease title and year")
 
         for result in IMDb().search_movie(movie.title):
             if result.data['year'] == movie.year:
                 return self.create(id=result.movieID, movie=movie)
+
+        raise ValueError(f"No IMDb movies found for {movie}")
 
 
 class ImdbPersonManager(models.Manager):
@@ -45,7 +47,7 @@ class ImdbPersonManager(models.Manager):
 
     def create_for(self, person):
         if not person.first_name or not person.last_name:
-            raise ValueError("To be able search person in IMDb it should has first name and last name")
+            raise ValueError("To be able search IMDb person it should has first name and last name")
 
         imdb = IMDb()
 
@@ -62,6 +64,8 @@ class ImdbPersonManager(models.Manager):
         for result in imdb.search_person(person.name):
             # TODO: make more complicated check if it's right person
             return self.create(id=result.personID, person=person)
+
+        raise ValueError(f"No IMDb persons found for {person}")
 
 
 class ImdbMovie(models.Model, UrlMixin):
