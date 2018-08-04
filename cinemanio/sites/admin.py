577e151a335c099c47857fa7de08702931259be8
ramusus <ramusus@gmail.com>
2018-08-04 15:42:53 -0400

    display wikipedia titles on movie and person pages in admin

--- cinemanio/sites/admin.py
+++ cinemanio/sites/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.contrib.admin import register, TabularInline
+from django.db.models import Prefetch
 
 from cinemanio.core.admin import get_registered_admin_class
 from cinemanio.core.models import Movie, Person
 from cinemanio.sites.imdb.models import ImdbMovie, ImdbPerson
 from cinemanio.sites.kinopoisk.models import KinopoiskMovie, KinopoiskPerson
+from cinemanio.sites.wikipedia.models import WikipediaPage
 
 MovieAdmin = get_registered_admin_class(Movie)
 PersonAdmin = get_registered_admin_class(Person)
@@ -32,33 +34,50 @@ class KinopoiskPersonInline(InlineBase):
     model = KinopoiskPerson
 
 
-@register(Movie)
-class SitesMovieAdmin(MovieAdmin):
-    list_display = MovieAdmin.list_display + ['imdb_id', 'imdb_rating', 'kinopoisk_id', 'kinopoisk_rating']
-    list_select_related = ('imdb', 'kinopoisk')
-    inlines = [ImdbMovieInline, KinopoiskMovieInline] + MovieAdmin.inlines
-
+class SitesAdminMixin:
     def imdb_id(self, obj):
         return obj.imdb.id
 
-    def imdb_rating(self, obj):
-        return obj.imdb.rating
-
     def kinopoisk_id(self, obj):
         return obj.kinopoisk.id
+
+    def wikipedia_en(self, obj):
+        return self.wikipedia(obj, 'en')
+
+    def wikipedia_ru(self, obj):
+        return self.wikipedia(obj, 'ru')
+
+    def wikipedia(self, obj, lang):
+        try:
+            return getattr(obj, f'wikipedia_{lang}')[0].name
+        except IndexError:
+            return ''
+
+    def get_queryset(self, request):
+        return (super().get_queryset(request)
+            .prefetch_related(
+            Prefetch('wikipedia', to_attr='wikipedia_en', queryset=WikipediaPage.objects.filter(lang='en')))
+            .prefetch_related(
+            Prefetch('wikipedia', to_attr='wikipedia_ru', queryset=WikipediaPage.objects.filter(lang='ru')))
+        )
+
+
+@register(Movie)
+class SitesMovieAdmin(SitesAdminMixin, MovieAdmin):
+    list_display = MovieAdmin.list_display + ['imdb_id', 'imdb_rating', 'kinopoisk_id', 'kinopoisk_rating',
+                                              'wikipedia_en', 'wikipedia_ru']
+    list_select_related = ('imdb', 'kinopoisk')
+    inlines = [ImdbMovieInline, KinopoiskMovieInline] + MovieAdmin.inlines
+
+    def imdb_rating(self, obj):
+        return obj.imdb.rating
 
     def kinopoisk_rating(self, obj):
         return obj.kinopoisk.rating
 
 
 @register(Person)
-class SitesPersonAdmin(PersonAdmin):
-    list_display = PersonAdmin.list_display + ['imdb_id', 'kinopoisk_id']
+class SitesPersonAdmin(SitesAdminMixin, PersonAdmin):
+    list_display = PersonAdmin.list_display + ['imdb_id', 'kinopoisk_id', 'wikipedia_en', 'wikipedia_ru']
     list_select_related = ('imdb', 'kinopoisk')
     inlines = [ImdbPersonInline, KinopoiskPersonInline] + PersonAdmin.inlines
-
-    def imdb_id(self, obj):
-        return obj.imdb.id
-
-    def kinopoisk_id(self, obj):
-        return obj.kinopoisk.id
